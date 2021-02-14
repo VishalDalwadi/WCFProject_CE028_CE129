d55c6902d65aec7d84a32ea5190eb358c4214106
Vishal Dalwadi <dalwadivishal26@gmail.com>
2021-02-14 23:21:56 +0530

    Implemented AuthenticationService

---
+++ AuthenticationService/AuthenticationService.cs
@@ -0,0 +1,82 @@
+﻿using JWT.Algorithms;
+using JWT.Builder;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.ServiceModel;
+using System.Text;
+
+namespace AuthenticationService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AuthenticationService" in both code and config file together.
+    public class AuthenticationService : IAuthenticationService
+    {
+        string IAuthenticationService.AreCorrectCredentials(string username, string password)
+        {
+            string token = "";
+            bool correct_password = true;
+            bool user_found = true;
+            string connectionString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM Users WHERE username = @username", conn);
+                command.Parameters.AddWithValue("@username", username);
+                conn.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    HashAlgorithm hashAlgorithm = new SHA512Managed();
+                    byte[] salt = (byte[])reader["salt"];
+                    byte[] hashed_password = (byte[])reader["hashed_password"];
+                    byte[] password_bytes = Encoding.ASCII.GetBytes(password);
+                    byte[] pt_password = new byte[salt.Length + password_bytes.Length];
+                    byte[] h_password;
+                    password_bytes.CopyTo(pt_password, 0);
+                    salt.CopyTo(pt_password, password.Length);
+                    h_password = hashAlgorithm.ComputeHash(pt_password);
+                    for (int i = 0; i < h_password.Length; i++)
+                    {
+                        if (h_password[i] != hashed_password[i])
+                        {
+                            correct_password = false;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    user_found = false;
+                }
+
+                if (user_found && correct_password)
+                {
+                    token = new JwtBuilder()
+                        .WithAlgorithm(new HMACSHA256Algorithm())
+                        .WithSecret(ConfigurationManager.AppSettings["secret_key"])
+                        .AddClaim("_id", reader["_id"])
+                        .AddClaim("username", reader["username"])
+                        .AddClaim("email_id", reader["email_id"])
+                        .Encode();
+                } 
+                else
+                {
+                    if (!user_found)
+                    {
+                        throw new FaultException<AuthenticationFault>(AuthenticationFault.NoSuchUser);
+                    }
+                    else if (!correct_password)
+                    {
+                        throw new FaultException<AuthenticationFault>(AuthenticationFault.InvalidPassword);
+                    } else
+                    {
+                        throw new FaultException<AuthenticationFault>(AuthenticationFault.ServerFault);
+                    }
+                }
+            }
+
+            return token;
+        }
+    }
+}
