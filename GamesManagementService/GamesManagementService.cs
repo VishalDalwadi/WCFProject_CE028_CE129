204092292aa2fc55d3568bd7d9fca9f64eab3832
Vishal Dalwadi <dalwadivishal26@gmail.com>
2021-02-15 01:00:54 +0530

    Implemented GamesManagementService

---
+++ GamesManagementService/GamesManagementService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GamesManagementService
+{
+    // TODO: Reference AuthortizationService and authorize user. Get the user_id. For now, token = user_id.
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "GamesManagementService" in both code and config file together.
+    public class GamesManagementService : IGamesManagementService
+    {
+        void IGamesManagementService.SaveGame(Game game, string token)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("INSERT INTO SavedGames (user_id, game_string, played_as) VALUES (@user_id, @game_string, @played_as)", conn);
+                command.Parameters.AddWithValue("@user_id", token);
+                command.Parameters.AddWithValue("@game_string", game.GameString);
+                command.Parameters.AddWithValue("@played_as", game.PlayedAs == Game.Player.White ? 'W' : 'B');
+                conn.Open();
+                command.ExecuteNonQuery();
+            }
+
+        }
+
+        List<Game> IGamesManagementService.GetAllSavedGames(string token)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+            List<Game> games = new List<Game>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("SELECT game_string, played_as FROM SavedGames WHERE user_id = @user_id", conn);
+                command.Parameters.AddWithValue("@user_id", token);
+                conn.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    games.Add(new Game((string)reader["game_string"], (string)reader["played_as"] == "W" ? Game.Player.White : Game.Player.Black));
+                }
+            }
+
+            return games;
+        }
+    }
+}
---
+++ GamesManagementService/IGamesManagementService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GamesManagementService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IGamesManagementService" in both code and config file together.
+    [ServiceContract]
+    public interface IGamesManagementService
+    {
+        [OperationContract]
+        void SaveGame(Game game, string token);
+
+        [OperationContract]
+        List<Game> GetAllSavedGames(string token);
+    }
+
+    [DataContract]
+    public class Game
+    {
+        public enum Player
+        {
+            White,
+            Black
+        }
+
+        private Player _played_as;
+        private string _game_string;
+
+        [DataMember]
+        public Player PlayedAs
+        {
+            get { return _played_as; }
+            set { _played_as = value; }
+        }
+
+        [DataMember]
+        public string GameString
+        {
+            get { return _game_string; }
+            set { _game_string = value; }
+        }
+
+        public Game() { }
+        public Game(string game_string, Player played_as)
+        {
+            _game_string = game_string;
+            _played_as = played_as;
+        }
+    }
+}
