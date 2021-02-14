5a3dd40ce33066ac83d5be107e6e7f987dbd7da5
Vishal Dalwadi <dalwadivishal26@gmail.com>
2021-02-14 22:23:33 +0530

    Implemented the Registration Service

---
+++ RegistrationService/IRegistrationService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RegistrationService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRegistrationService" in both code and config file together.
+    [ServiceContract]
+    public interface IRegistrationService
+    {
+        [OperationContract]
+        void RegisterUser(User user);
+
+        [OperationContract]
+        bool IsUsernameTaken(string username);
+
+        [OperationContract]
+        bool UserWithEmailIdExists(string email_id);
+    }
+
+    [DataContract]
+    public class User
+    {
+        private string _username;
+        private string _email_id;
+        private string _password;
+
+        [DataMember]
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        [DataMember]
+        public string EmailID
+        {
+            get { return _email_id; }
+            set { _email_id = value; }
+        }
+
+        [DataMember]
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+    }
+}
---
+++ RegistrationService/RegistrationService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RegistrationService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RegistrationService" in both code and config file together.
+    public class RegistrationService : IRegistrationService
+    {
+        void IRegistrationService.RegisterUser(User user)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            HashAlgorithm hashAlgorithm = new SHA512Managed();
+            byte[] salt = new byte[32];
+            byte[] password = Encoding.ASCII.GetBytes(user.Password);
+            byte[] pt_password = new byte[salt.Length + password.Length];
+            byte[] hashed_password;
+            password.CopyTo(pt_password, 0);
+            rng.GetBytes(salt);
+            salt.CopyTo(pt_password, password.Length);
+            hashed_password = hashAlgorithm.ComputeHash(pt_password);
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("INSERT INTO Users (username, email_id, hashed_password, salt) VALUES (@username, @email_id, @hashed_password, @salt)", conn);
+                command.Parameters.AddWithValue("@username", user.Username);
+                command.Parameters.AddWithValue("@email_id", user.EmailID);
+                command.Parameters.Add("@hashed_password", System.Data.SqlDbType.Binary, hashed_password.Length).Value = hashed_password;
+                command.Parameters.Add("@salt", System.Data.SqlDbType.Binary, salt.Length).Value = salt;
+                conn.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        bool IRegistrationService.IsUsernameTaken(string username)
+        {
+            bool returnVal = false;
+            string connectionString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("SELECT username FROM Users WHERE username = @username", conn);
+                command.Parameters.AddWithValue("@username", username);
+                conn.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    returnVal = true;
+                }
+            }
+
+            return returnVal;
+        }
+
+        bool IRegistrationService.UserWithEmailIdExists(string email_id)
+        {
+            bool returnVal = false;
+            string connectionString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("SELECT username FROM Users WHERE email_id = @email_id", conn);
+                command.Parameters.AddWithValue("@email_id", email_id);
+                conn.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    returnVal = true;
+                }
+            }
+
+            return returnVal;
+        }
+    }
+}
