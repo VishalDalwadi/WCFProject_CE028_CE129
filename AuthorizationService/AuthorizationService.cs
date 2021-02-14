a4573b17940225c188543688fbe666d62b636cff
Vishal Dalwadi <dalwadivishal26@gmail.com>
2021-02-14 23:58:49 +0530

    Implemented AuthorizationService

---
+++ AuthorizationService/AuthorizationService.cs
@@ -0,0 +1,44 @@
+﻿using JWT.Algorithms;
+using JWT.Builder;
+using JWT.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AuthorizationService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AuthorizationService" in both code and config file together.
+    public class AuthorizationService : IAuthorizationService
+    {
+        User IAuthorizationService.AuthorizeUser(string token)
+        {
+            User user = new User();
+
+            try
+            {
+                var claims = new JwtBuilder()
+                .WithAlgorithm(new HMACSHA256Algorithm())
+                .WithSecret(ConfigurationManager.AppSettings["secret_key"])
+                .MustVerifySignature()
+                .Decode<IDictionary<string, object>>(token);
+                user.Id = (long)claims["_id"];
+                user.Username = (string)claims["username"];
+                user.Email_Id = (string)claims["email_id"];
+            }
+            catch (TokenExpiredException)
+            {
+                throw new FaultException<AuthorizationFault>(AuthorizationFault.TokenExpired);
+            }
+            catch (SignatureVerificationException)
+            {
+                throw new FaultException<AuthorizationFault>(AuthorizationFault.InvalidSignature);
+            }
+
+            return user;
+        }
+    }
+}
---
+++ AuthorizationService/IAuthorizationService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AuthorizationService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IAuthorizationService" in both code and config file together.
+    [ServiceContract]
+    public interface IAuthorizationService
+    {
+        [OperationContract]
+        [FaultContract(typeof(AuthorizationFault))]
+        User AuthorizeUser(string token);
+    }
+
+    [DataContract]
+    public class User
+    {
+        private Int64 _id;
+        private string _username;
+        private string _email_id;
+
+        [DataMember]
+        public Int64 Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        [DataMember]
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        [DataMember]
+        public string Email_Id
+        {
+            get { return _email_id; }
+            set { _email_id = value; }
+        }
+    }
+
+    [DataContract]
+    public enum AuthorizationFault
+    {
+        TokenExpired,
+        InvalidSignature
+    }
+}
