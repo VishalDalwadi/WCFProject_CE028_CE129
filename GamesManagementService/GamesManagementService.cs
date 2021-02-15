37a6fbc11e8e2e6d7feada0fd70c227d8c67ff47
Vishal Dalwadi <dalwadivishal26@gmail.com>
2021-02-15 15:54:39 +0530

    Modified GamesManagementService to authorize users (Faults not handled)

--- GamesManagementService/GamesManagementService.cs
+++ GamesManagementService/GamesManagementService.cs
@@ -9,18 +9,22 @@ using System.Text;
 
 namespace GamesManagementService
 {
-    // TODO: Reference AuthortizationService and authorize user. Get the user_id. For now, token = user_id.
-
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "GamesManagementService" in both code and config file together.
     public class GamesManagementService : IGamesManagementService
     {
         void IGamesManagementService.SaveGame(Game game, string token)
         {
+            AuthorizationServiceReference.User user;
+            using (AuthorizationServiceReference.AuthorizationServiceClient client = new AuthorizationServiceReference.AuthorizationServiceClient())
+            {
+                user = client.AuthorizeUser(token);
+            }
+
             string connectionString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 SqlCommand command = new SqlCommand("INSERT INTO SavedGames (user_id, game_string, played_as) VALUES (@user_id, @game_string, @played_as)", conn);
-                command.Parameters.AddWithValue("@user_id", token);
+                command.Parameters.AddWithValue("@user_id", user.Id);
                 command.Parameters.AddWithValue("@game_string", game.GameString);
                 command.Parameters.AddWithValue("@played_as", game.PlayedAs == Game.Player.White ? 'W' : 'B');
                 conn.Open();
@@ -31,13 +35,19 @@ namespace GamesManagementService
 
         List<Game> IGamesManagementService.GetAllSavedGames(string token)
         {
+            AuthorizationServiceReference.User user;
+            using (AuthorizationServiceReference.AuthorizationServiceClient client = new AuthorizationServiceReference.AuthorizationServiceClient())
+            {
+                user = client.AuthorizeUser(token);
+            }
+
             string connectionString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
             List<Game> games = new List<Game>();
 
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 SqlCommand command = new SqlCommand("SELECT game_string, played_as FROM SavedGames WHERE user_id = @user_id", conn);
-                command.Parameters.AddWithValue("@user_id", token);
+                command.Parameters.AddWithValue("@user_id", user.Id);
                 conn.Open();
                 SqlDataReader reader = command.ExecuteReader();
 
