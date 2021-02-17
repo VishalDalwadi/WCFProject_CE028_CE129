4b399ef5a252a2bdb33875ba0ef79f8945044d3f
Vishal Dalwadi <51291657+VishalDalwadi@users.noreply.github.com>
2021-02-17 12:42:36 +0530

    Revert "Games management services"

--- GamesManagementService/IGamesManagementService.cs
+++ GamesManagementService/IGamesManagementService.cs
@@ -18,14 +18,9 @@ namespace GamesManagementService
         [OperationContract]
         [FaultContract(typeof(GamesManagementFault))]
         List<Game> GetAllSavedGames(string token);
-        
-        [OperationContract]
-        [FaultContract(typeof(GamesManagementFault))]
-        void DeleteGame(Game game, string token);
 
         [OperationContract]
-        [FaultContract(typeof(GamesManagementFault))]
-        string FindMatch(string token);
+        void DeleteGame(Game game, string token);
     }
 
     [DataContract]
