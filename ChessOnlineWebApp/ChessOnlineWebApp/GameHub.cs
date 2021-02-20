8a84422848698fa22da004fe56afa9baf2882217
Vishal Dalwadi <dalwadivishal26@gmail.com>
2021-02-20 21:22:57 +0530

    Created GameHub and included SignalR Scripts

---
+++ ChessOnlineWebApp/ChessOnlineWebApp/GameHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ChessOnlineWebApp
+{
+    public class GameHub : Hub
+    {
+        private GamesManagementServiceReference.GamesManagementServiceClient _gms_client;
+
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            _gms_client.Close();
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
